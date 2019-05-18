0d24f33c47dea0d514707742a7708d1aea1ae5d6
Mat Gargano <mgargano@gmail.com>
2019-05-18 19:05:31 -0400

    update

--- site/config/application.php
+++ site/config/application.php
@@ -11,7 +11,7 @@
 use Roots\WPConfig\Config;
 
 /** @var string Directory containing all of the site's files */
-$root_dir = dirname(__DIR__);
+$root_dir = dirname( __DIR__ );
 
 /** @var string Document Root */
 $webroot_dir = $root_dir . '/web';
@@ -24,93 +24,97 @@ Env::init();
 /**
  * Use Dotenv to set required environment variables and load .env file in root
  */
-$dotenv = Dotenv\Dotenv::create($root_dir);
-if (file_exists($root_dir . '/.env')) {
+$dotenv = Dotenv\Dotenv::create( $root_dir );
+if ( file_exists( $root_dir . '/.env' ) ) {
     $dotenv->load();
-    $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL']);
+    $dotenv->required( [ 'DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL' ] );
 }
 
 /**
  * Set up our global environment constant and load its config first
  * Default: production
  */
-define('WP_ENV', env('WP_ENV') ?: 'production');
+define( 'WP_ENV', env( 'WP_ENV' ) ?: 'production' );
 
 /**
  * URLs
  */
-Config::define('WP_HOME', env('WP_HOME'));
-Config::define('WP_SITEURL', env('WP_SITEURL'));
+Config::define( 'WP_HOME', env( 'WP_HOME' ) );
+Config::define( 'WP_SITEURL', env( 'WP_SITEURL' ) );
 
 /**
  * Custom Content Directory
  */
-Config::define('CONTENT_DIR', '/app');
-Config::define('WP_CONTENT_DIR', $webroot_dir . Config::get('CONTENT_DIR'));
-Config::define('WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_DIR'));
+Config::define( 'CONTENT_DIR', '/app' );
+Config::define( 'WP_CONTENT_DIR', $webroot_dir . Config::get( 'CONTENT_DIR' ) );
+Config::define( 'WP_CONTENT_URL', Config::get( 'WP_HOME' ) . Config::get( 'CONTENT_DIR' ) );
 
 /**
  * DB settings
  */
-Config::define('DB_NAME', env('DB_NAME'));
-Config::define('DB_USER', env('DB_USER'));
-Config::define('DB_PASSWORD', env('DB_PASSWORD'));
-Config::define('DB_HOST', env('DB_HOST') ?: 'localhost');
-Config::define('DB_CHARSET', 'utf8mb4');
-Config::define('DB_COLLATE', '');
-$table_prefix = env('DB_PREFIX') ?: 'wp_';
+Config::define( 'DB_NAME', env( 'DB_NAME' ) );
+Config::define( 'DB_USER', env( 'DB_USER' ) );
+Config::define( 'DB_PASSWORD', env( 'DB_PASSWORD' ) );
+Config::define( 'DB_HOST', env( 'DB_HOST' ) ?: 'localhost' );
+Config::define( 'DB_CHARSET', 'utf8mb4' );
+Config::define( 'DB_COLLATE', '' );
+$table_prefix = env( 'DB_PREFIX' ) ?: 'wp_';
 
 /**
  * Authentication Unique Keys and Salts
  */
-Config::define('AUTH_KEY', env('AUTH_KEY'));
-Config::define('SECURE_AUTH_KEY', env('SECURE_AUTH_KEY'));
-Config::define('LOGGED_IN_KEY', env('LOGGED_IN_KEY'));
-Config::define('NONCE_KEY', env('NONCE_KEY'));
-Config::define('AUTH_SALT', env('AUTH_SALT'));
-Config::define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
-Config::define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
-Config::define('NONCE_SALT', env('NONCE_SALT'));
+Config::define( 'AUTH_KEY', env( 'AUTH_KEY' ) );
+Config::define( 'SECURE_AUTH_KEY', env( 'SECURE_AUTH_KEY' ) );
+Config::define( 'LOGGED_IN_KEY', env( 'LOGGED_IN_KEY' ) );
+Config::define( 'NONCE_KEY', env( 'NONCE_KEY' ) );
+Config::define( 'AUTH_SALT', env( 'AUTH_SALT' ) );
+Config::define( 'SECURE_AUTH_SALT', env( 'SECURE_AUTH_SALT' ) );
+Config::define( 'LOGGED_IN_SALT', env( 'LOGGED_IN_SALT' ) );
+Config::define( 'NONCE_SALT', env( 'NONCE_SALT' ) );
 
 /**
  * Custom Settings
  */
-Config::define('AUTOMATIC_UPDATER_DISABLED', true);
-Config::define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
+Config::define( 'AUTOMATIC_UPDATER_DISABLED', true );
+Config::define( 'DISABLE_WP_CRON', env( 'DISABLE_WP_CRON' ) ?: false );
 // Disable the plugin and theme file editor in the admin
-Config::define('DISALLOW_FILE_EDIT', true);
+Config::define( 'DISALLOW_FILE_EDIT', true );
 // Disable plugin and theme updates and installation from the admin
-Config::define('DISALLOW_FILE_MODS', true);
+Config::define( 'DISALLOW_FILE_MODS', true );
 // Allow Multisite
-Config::define('WP_ALLOW_MULTISITE', true);
+Config::define( 'WP_ALLOW_MULTISITE', true );
 
 /**
  * Multisite
  *
  * TODO: Use \Roots\Config with these
  */
-define('MULTISITE', true);
-define('SUBDOMAIN_INSTALL', true);
-define('DOMAIN_CURRENT_SITE', env('DOMAIN_CURRENT_SITE'));
-define('PATH_CURRENT_SITE', env('PATH_CURRENT_SITE') ?: '/');
-define('SITE_ID_CURRENT_SITE', env('SITE_ID_CURRENT_SITE') ?: 1);
-define('BLOG_ID_CURRENT_SITE', env('BLOG_ID_CURRENT_SITE') ?: 1);
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', true );
+define( 'DOMAIN_CURRENT_SITE', env( 'DOMAIN_CURRENT_SITE' ) );
+define( 'PATH_CURRENT_SITE', env( 'PATH_CURRENT_SITE' ) ?: '/' );
+define( 'SITE_ID_CURRENT_SITE', env( 'SITE_ID_CURRENT_SITE' ) ?: 1 );
+define( 'BLOG_ID_CURRENT_SITE', env( 'BLOG_ID_CURRENT_SITE' ) ?: 1 );
 
 /**
  * Multisite cookie fix
  */
-Config:define('COOKIE_DOMAIN', $_SERVER['HTTP_HOST']);
+
+if ( array_key_exists( 'HTTP_HOST', $_SERVER ) ) {
+
+    Config:define( 'COOKIE_DOMAIN', $_SERVER['HTTP_HOST'] );
+}
 
 /**
  * Debugging Settings
  */
-Config::define('WP_DEBUG_DISPLAY', false);
-Config::define('SCRIPT_DEBUG', false);
-ini_set('display_errors', 0);
+Config::define( 'WP_DEBUG_DISPLAY', false );
+Config::define( 'SCRIPT_DEBUG', false );
+ini_set( 'display_errors', 0 );
 
 $env_config = __DIR__ . '/environments/' . WP_ENV . '.php';
 
-if (file_exists($env_config)) {
+if ( file_exists( $env_config ) ) {
     require_once $env_config;
 }
 
@@ -119,6 +123,6 @@ Config::apply();
 /**
  * Bootstrap WordPress
  */
-if (!defined('ABSPATH')) {
-    define('ABSPATH', $webroot_dir . '/wp/');
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', $webroot_dir . '/wp/' );
 }
